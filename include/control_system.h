cefa53ce8aaef2baa15f01f8d17ca8fee6fb43a8
izasoerya <ihza.surya@gmail.com>
2025-06-15 15:46:25 +0700

    feat: implement run automatic method

--- include/control_system.h
+++ include/control_system.h
@@ -16,6 +16,7 @@ class ControlSystem
 private:
     const float offsetAnglePositive = 2;
     const float offsetAngleNegative = 2;
+    const float automaticSpeed = 5;
     Motor motorX = driverX;
     Motor motorY = driverY;
 
@@ -77,5 +78,34 @@ void ControlSystem::runManual(float axisX, float axisY, float roll, float pitch)
         {
             motorY.stop();
         }
+    }
+}
+
+void ControlSystem::runAutomatic(float centerVectorX, float centerVectorY)
+{
+    if (centerVectorX > 0.1)
+    {
+        motorX.turnLeft(automaticSpeed);
+    }
+    else if (centerVectorX < -0.1)
+    {
+        motorX.turnRight(automaticSpeed);
+    }
+    else
+    {
+        motorX.stop();
+    }
+
+    if (centerVectorY > 0.1)
+    {
+        motorY.turnLeft(automaticSpeed);
+    }
+    else if (centerVectorY < -0.1)
+    {
+        motorY.turnRight(automaticSpeed);
+    }
+    else
+    {
+        motorY.stop();
     }
 }
