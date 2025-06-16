7bb08b2b4fa59c84d7de7cabc8e6315c39ba58a1
izasoerya <ihza.surya@gmail.com>
2025-06-16 12:21:19 +0700

    feat: refactor to head and body object

--- include/motor.h
+++ include/motor.h
@@ -1,75 +1,118 @@
+#ifndef SENSOR_LDR_H
+#define SENSOR_LDR_H
 
-/** GENERAL DESCRIPTION
- * @brief Assume to use Driver BTS7960 for control motor
- * PIN is defined at the implementation
- */
-
-#pragma once
 #include <Arduino.h>
+#include <Wire.h>
 
-class Motor
+/**
+ * @brief SensorLDR class for reading LDR sensor values using weighted vector readings.
+ *
+ * This class reads analog values from an array of LDR sensors and calculates
+ * normalized X and Y centroids based on weighted values.
+ */
+class SensorLDR
 {
 private:
-    byte LEN;
-    byte REN;
-    byte LPWM;
-    byte RPWM;
+    byte pin[6];
+    byte value[6] = {0, 0, 0, 0, 0, 0};
+    float weightedVectorX[6] = {-1, 0, 1, -1, 0, 1};
+    float weightedVectorY[6] = {-1, -1, -1, 1, 1, 1};
 
 public:
     /**
-     * @brief Construct a new BTS7960 Driver Motor object
-     *
-     * @param REN Enable pin for right PWM
-     * @param LEN Enable pin for left PWM
-     * @param RPWM PWM pin for control PWM right
-     * @param LPWM PWM pin for control PWM left
+     * @brief Constructs a SensorLDR object.
+     * @param pins An array of 6 pin numbers connected to the LDR sensors.
      */
-    Motor(byte REN, byte LEN, byte RPWM, byte LPWM)
-    {
-        this->LEN = LEN;
-        this->REN = REN;
-        this->LPWM = LPWM;
-        this->RPWM = RPWM;
-        pinMode(LEN, OUTPUT);
-        pinMode(REN, OUTPUT);
-        digitalWrite(LEN, LOW);
-        digitalWrite(REN, LOW);
-    };
-
-    ~Motor() {};
+    SensorLDR(byte pins[6]);
 
     /**
-     * @brief Turns the motor to the left with the specified speed.
-     *
-     * @param speed The speed at which to run the motor (0 - 255).
+     * @brief Initializes the sensor pins.
      */
-    void turnLeft(byte speed)
-    {
-        analogWrite(LPWM, 0);
-        digitalWrite(LPWM, LOW);
-        analogWrite(RPWM, speed);
-    }
+    void begin();
 
     /**
-     * @brief Turns the motor to the right with the specified speed.
-     *
-     * @param speed The speed at which to run the motor (0 - 255).
+     * @brief Updates the sensor readings.
      */
-    void turnRight(byte speed)
-    {
-        analogWrite(RPWM, 0);
-        digitalWrite(RPWM, LOW);
-        analogWrite(LPWM, speed);
-    }
+    void update();
 
     /**
-     * @brief Stops the motor.
+     * @brief Returns the raw analog value at the specified index.
+     * @param index The sensor index.
+     * @return uint16_t The raw sensor value.
      */
-    void stop()
-    {
-        analogWrite(RPWM, 0);
-        digitalWrite(RPWM, LOW);
-        analogWrite(LPWM, 0);
-        digitalWrite(LPWM, 0);
-    }
+    uint16_t getRawValue(int index);
+
+    /**
+     * @brief Calculates the normalized X centroid using weighted values.
+     * @return float The normalized X value.
+     */
+    float getNormalizedX();
+
+    /**
+     * @brief Calculates the normalized Y centroid using weighted values.
+     * @return float The normalized Y value.
+     */
+    float getNormalizedY();
 };
+
+#endif // SENSOR_LDR_H
+
+// ------------------------------
+// Implementation Section
+// ------------------------------
+
+SensorLDR::SensorLDR(byte pins[6])
+{
+    memcpy(this->pin, pins, sizeof(this->pin));
+}
+
+void SensorLDR::begin()
+{
+    for (int i = 0; i < 6; i++)
+    {
+        pinMode(pin[i], INPUT);
+    }
+}
+
+void SensorLDR::update()
+{
+    for (int i = 0; i < 6; i++)
+    {
+        value[i] = analogRead(pin[i]);
+    }
+}
+
+uint16_t SensorLDR::getRawValue(int index)
+{
+    return value[index];
+}
+
+float SensorLDR::getNormalizedX()
+{
+    float sumX = 0;
+    float totalSum = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        float val = static_cast<float>(value[i]);
+        sumX += val * weightedVectorX[i];
+        totalSum += val;
+    }
+    if (totalSum == 0)
+        return 0;
+    return sumX / totalSum;
+}
+
+float SensorLDR::getNormalizedY()
+{
+    float sumY = 0;
+    float totalSum = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        float val = static_cast<float>(value[i]);
+        sumY += val * weightedVectorY[i];
+        totalSum += val;
+    }
+    if (totalSum == 0)
+        return 0;
+    return sumY / totalSum;
+}
